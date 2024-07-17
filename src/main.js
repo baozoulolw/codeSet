2011dcb8994e56753b6669878e15a170ae6b4a5e
WangHr <Baozoulolw@gmail.com>
2024-07-17 09:05:22 +0800

    监听dom生成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp, ref } from 'vue';
+import { waitForElementById } from './utils/utils';
 import './style.css';
 import App from './App.vue';
 // 引入组件库的少量全局样式变量
 import 'tdesign-vue-next/es/style/index.css';
 
-let app = createApp(App)
+
 const render = (el) => {
   createApp(App).mount(
     (() => {
@@ -15,13 +16,14 @@ const render = (el) => {
     })(),
   );
 }
-
-let icon = document.querySelector('#tab-code')
-icon.addEventListener('click', async function () {
-  let mask = document.querySelector('.code.mask_popup')
-  if (!mask) return
-  let dialog = mask.querySelector('#self_dialog')
-  if (!dialog) {
-    render(mask)
-  }
+waitForElementById('tab-code', icon => {
+  // let icon = document.querySelector('#tab-code')
+    icon.addEventListener('click', async function () {
+    let mask = document.querySelector('.code.mask_popup')
+    if (!mask) return
+    let dialog = mask.querySelector('#self_dialog')
+    if (!dialog) {
+      render(mask)
+    }
+  })
 })
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -15,3 +15,17 @@ export const wait = async (time = 2000) => {
   await new Promise(wait => setTimeout(wait, time))
 }
 
+export function waitForElementById(id, callback) {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      Array.from(mutation.addedNodes).forEach(addedNode => {
+        if (addedNode.id === id) {
+          observer.disconnect(); // 停止观察，因为我们找到了目标元素
+          callback(addedNode); // 触发回调，传递找到的节点
+        }
+      });
+    });
+  });
+  observer.observe(document.body, { childList: true, subtree: true }); // 监听body下所有新增的节点
+}
+
